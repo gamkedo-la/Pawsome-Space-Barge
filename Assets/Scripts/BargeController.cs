eb81be1e559dd27c86ed204df2508bc0b8be86ff
Evan Sklarski <esklarski@gmail.com>
2022-02-16 01:19:30 -0800

    Move barge position with RigidBody2D.MovePosition()

--- Assets/Scripts/BargeController.cs
+++ Assets/Scripts/BargeController.cs
@@ -7,18 +7,19 @@ public class BargeController : MonoBehaviour
     [HideInInspector] private Rigidbody2D rb2d;
 
     [Range(-1,1)]
-    [SerializeField] private float movementCoefficient = 0.5f;
+    [SerializeField] private float movementCoefficient = -0.3f;
 
     void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();       
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = (
+        Vector2 newPosition =
             rb2d.position +
-            new Vector2(0, AsteroidField.instance.fieldSpeed * movementCoefficient * Time.deltaTime)
-        );
+            new Vector2(0, AsteroidField.instance.fieldSpeed * movementCoefficient * Time.deltaTime);
+
+        rb2d.MovePosition(newPosition);
     }
 }
