949566751377520927547928cf548463c01d4c52
Evan Sklarski <esklarski@gmail.com>
2022-02-28 11:38:39 -0800

    Commenting, tweaks, and cleanup.

--- Assets/Scripts/PlayerShip/PlayerFollowCamera.cs
+++ Assets/Scripts/PlayerShip/PlayerFollowCamera.cs
@@ -1,59 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Follows transform, and rotates with it.
+/// Has the effect of looking in front of the player heading letting the player drift to one side of the frame.
+/// Helps keep camera focused on what's coming.
+/// </summary>
 public class PlayerFollowCamera : MonoBehaviour
 {
-    [SerializeField] private Transform player;
+    [Tooltip("Transform of player ship.")]
+    [SerializeField]
+    private Transform player;
 
-    // for Follow Camera
-    [SerializeField] private Vector3 positionOffset = new Vector3(0f, 10f, -80f);
-    [SerializeField] private Vector3 angleOffset = new Vector3(0f, 70f, -90f);
+    [Tooltip("Position offset from Player ship.")]
+    [SerializeField] private Vector3 positionOffset = new Vector3(0f, 10f, -70f);
+
+    [Tooltip("Angle offset from player ship.")]
+    [SerializeField] private Vector3 angleOffset = new Vector3(0f, 80f, -90f);
 
     [Tooltip("Damping factor to smooth the changes in camera position.")]
-    [SerializeField] private float motionDamping = 10f;
+    [SerializeField] private float motionDamping = 5f;
 
     [Tooltip("Damping factor to smooth the changes in camera position.")]
-    [SerializeField] private float rotationDamping = 0.6f;
+    [SerializeField] private float rotationDamping = 1.5f;
 
     void LateUpdate()
     {
         // apply initialrotation
         Quaternion initialRotation = Quaternion.Euler(angleOffset);
 
-        // if (allowRotationTracking)
-        // {
-            Quaternion rot = Quaternion.Lerp(
-                transform.rotation,
-                player.rotation * initialRotation,
-                Time.deltaTime * rotationDamping
-            );
+        Quaternion rot = Quaternion
+            .Lerp(transform.rotation, player.rotation * initialRotation, Time.deltaTime * rotationDamping);
 
-            transform.rotation = rot;
-        // }
-        // else
-        // {
-        //     transform.rotation = Quaternion.RotateTowards(
-        //         transform.rotation,
-        //         initialRotation,
-        //         mDamping * Time.deltaTime);
-        // }
+        transform.rotation = rot;
         
-
         // calc transformed axes
         Vector3 forward = transform.rotation * Vector3.forward;
         Vector3 right = transform.rotation * Vector3.right;
         Vector3 up = transform.rotation * Vector3.up;
 
-        // calc coordinate fram offset
+        // calc coordinate from offset
         Vector3 targetPosition = player.position;
         Vector3 desiredPosition = targetPosition
-            + forward * positionOffset.z
-            + right * positionOffset.x
-            + up * positionOffset.y;
+                                    + forward * positionOffset.z
+                                    + right * positionOffset.x
+                                    + up * positionOffset.y;
 
         // change camera position
-        Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * motionDamping);
+        Vector3 position = Vector3
+            .Lerp(transform.position, desiredPosition, Time.deltaTime * motionDamping);
+
         transform.position = position;
     }
 }
