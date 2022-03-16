9967512f1f2aae88ff4bf1d1fd910df51a9c28d1
Johan Östling <johan.f.ostling@gmail.com>
2022-03-16 20:02:29 +0100

    Add a delivery window

---
+++ Assets/Scripts/Barge/DeliveryWindow.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class DeliveryWindow : MonoBehaviour
+{
+    public UnityEvent onBargeEnter;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Barge"))
+        {
+            Debug.Log("Delivery window hit!");
+            onBargeEnter.Invoke();
+        }
+    }
+}
