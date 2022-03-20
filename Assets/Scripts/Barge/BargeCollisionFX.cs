b4fcfc1059f692fb29cc3dbdb4cd3d0ea0b79a94
Evan Sklarski <esklarski@gmail.com>
2022-03-20 08:58:46 -0700

    Minor formatting, documantation.

--- Assets/Scripts/Barge/BargeCollisionFX.cs
+++ Assets/Scripts/Barge/BargeCollisionFX.cs
@@ -1,14 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BargeCollisionFX : MonoBehaviour
 {
+    // This should be on the parent object for correct function.
     private BargeCollision barge;
+
+
     private void Start()
     {
+        // get main collision effects script
         barge = gameObject.GetComponentInParent<BargeCollision>();
     }
+
+
     /// <summary>
     /// Catches particle system callback, disables parent object.'
     /// Part of simple object pool in BargeCollision.cs
