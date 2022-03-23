5336e0c49eaaade0c9ef905da55b7c4ca4326104
Evan Sklarski <esklarski@gmail.com>
2022-03-23 09:44:50 -0700

    Create script to hold obstacle avoidance code. Empty placeholder.

---
+++ Assets/Scripts/Enemy/EnemyNavigationSystem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Obstacle avoidance system.
+/// </summary>
+public class EnemyNavigationSystem : MonoBehaviour
+{
+    // empty for now
+}
