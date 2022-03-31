a14cf5a9ae67df0a5a448cf52a54e255a85d2aac
Evan Sklarski <esklarski@gmail.com>
2022-03-31 16:21:28 -0700

    Create ReadOnly property attribute.

---
+++ Assets/Scripts/Editor/ReadOnlyDrawer.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer: PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        var previousState = GUI.enabled;
+
+        GUI.enabled = false;
+
+        EditorGUI.PropertyField(position, property, label);
+
+        GUI.enabled = previousState;
+    }
+}
