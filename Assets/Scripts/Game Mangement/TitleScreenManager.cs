8fb67190f618c33463f951e972f55b1d966baac4
Evan Sklarski <esklarski@gmail.com>
2022-04-06 14:40:27 -0700

    Disable First Officer button while dialog open, reenable when done.

--- Assets/Scripts/Game Mangement/TitleScreenManager.cs
+++ Assets/Scripts/Game Mangement/TitleScreenManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fungus;
+using UnityEngine.UI;
 
 public class TitleScreenManager : MonoBehaviour
 {
     [SerializeField] private Flowchart kitty;
+    [SerializeField] private Button firstOfficerButton;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +21,20 @@ public class TitleScreenManager : MonoBehaviour
     }
 
     public void SeeKitty()
-    {   kitty.gameObject.SetActive(true);
+    {
+        kitty.gameObject.SetActive(true);
+        firstOfficerButton.interactable = false;
         kitty.SendFungusMessage("start");
+    }
+
+    public void ByeKitty()
+    {
+        StartCoroutine(FirstOfficerPause());
+    }
+
+    public IEnumerator FirstOfficerPause()
+    {
+        yield return new WaitForSeconds(0.5f);
+        firstOfficerButton.interactable = true;
     }
 }
