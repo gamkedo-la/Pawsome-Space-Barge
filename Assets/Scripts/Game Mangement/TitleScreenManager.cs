ff011177ef9950cfd7ccf4c288033d7c75545e7b
Evan Sklarski <esklarski@gmail.com>
2022-04-07 01:31:07 -0700

    Add basic flowchart for game settings and credits. Game can now be exited from title screen.

--- Assets/Scripts/Game Mangement/TitleScreenManager.cs
+++ Assets/Scripts/Game Mangement/TitleScreenManager.cs
@@ -3,23 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fungus;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class TitleScreenManager : MonoBehaviour
 {
-    [SerializeField] private Flowchart kitty;
-    [SerializeField] private Button firstOfficerButton;
+    [HideInInspector] public static TitleScreenManager Instance;
 
-    // Start is called before the first frame update
-    void Start()
+
+    private AsyncOperation loadOperation;
+    private bool loadComplete = false;
+    private TextMeshProUGUI playButtonText;
+    private ProgressBar progressBar;
+
+
+    [SerializeField, Tooltip("Settings flowchart.")]
+    private Flowchart kitty;
+
+    [SerializeField, Tooltip("Dialog call button. To be disabled when dialog open.")]
+    private Button firstOfficerButton;
+
+
+    [Header("Buttons")]
+    [SerializeField, Tooltip("Button to be disabled until load complete.")]
+    private Button playButton;
+
+    [SerializeField, Tooltip("Button text color when enabled.")]
+    private Color buttonEnabledTextColor;
+
+    [SerializeField, Tooltip("Button text color when disabled.")]
+    private Color buttonDisabledTextColor;
+
+    [SerializeField, Tooltip("Loading indicator text.")]
+    private TextMeshProUGUI loadingText;
+
+
+
+    // ********************************** Monobehaviours **********************************
+    private void Awake()
     {
+        Application.backgroundLoadingPriority = ThreadPriority.Low;
+
+        playButtonText = playButton.GetComponentInChildren<TextMeshProUGUI>();
+
+        progressBar = GetComponent<ProgressBar>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnEnable()
     {
-        
+        playButton.interactable = false;
+        playButtonText.color = buttonDisabledTextColor;
     }
 
+
+    private void Start()
+    {
+        System.Action loadCompleteAction = new System.Action(LoadingComplete);
+
+        loadOperation = SceneManager.LoadSceneAsync(1);
+        loadOperation.allowSceneActivation = false;
+
+        progressBar.StartLoading(loadOperation, loadCompleteAction);
+    }
+
+
+
+    // ********************************** Public Methods **********************************
     public void SeeKitty()
     {
         kitty.gameObject.SetActive(true);
@@ -27,12 +77,40 @@ public class TitleScreenManager : MonoBehaviour
         kitty.SendFungusMessage("start");
     }
 
+
     public void ByeKitty()
     {
         StartCoroutine(FirstOfficerPause());
     }
 
-    public IEnumerator FirstOfficerPause()
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+    public void PlayButtonClicked()
+    {
+        if (loadComplete)
+        {
+            loadOperation.allowSceneActivation = true;
+        }
+    }
+
+
+
+    // ********************************** Private Methods *********************************
+    private void LoadingComplete()
+    {
+        loadingText.enabled = false;
+        playButton.interactable = true;
+        playButtonText.color = buttonEnabledTextColor;
+        loadComplete = true;
+    }
+
+
+    private IEnumerator FirstOfficerPause()
     {
         yield return new WaitForSeconds(0.5f);
         firstOfficerButton.interactable = true;
