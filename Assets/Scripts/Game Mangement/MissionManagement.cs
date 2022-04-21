07d033b9d66df89b7b2942fd5d811ea292aae10d
Evan Sklarski <esklarski@gmail.com>
2022-04-21 00:30:24 -0700

    Move distance check for mission failure into MissionManagement loop.

--- Assets/Scripts/Game Mangement/MissionManagement.cs
+++ Assets/Scripts/Game Mangement/MissionManagement.cs
@@ -32,6 +32,8 @@ public class MissionManagement : MonoBehaviour
     }
 
 
+    private static float distanceCheckInterval = 2;
+    private float distanceCheckTimer = 0;
     private void Update()
     {
         if (GameManagement.Instance.enemyContactsCount > 0)
@@ -47,6 +49,22 @@ public class MissionManagement : MonoBehaviour
                 {
                     float force = purrtrolForce * Time.deltaTime * GameManagement.Instance.enemyContactsCount;
                     orrb.AddEnemyForce(force);
+                }
+            }
+        }
+
+        if (!GameManagement.Instance.DialogActive)
+        {
+            distanceCheckTimer += Time.deltaTime;
+
+            if (distanceCheckTimer > distanceCheckInterval)
+            {
+                distanceCheckTimer -= distanceCheckInterval;
+
+                if (Vector2.Distance(GameManagement.Instance.Barge.transform.position, Vector2.zero) > GameManagement.Instance.BargeOrbitalBody.MaxOrbitRadius * 0.8f)
+                {
+                    Debug.Log("Barge pushed out of orbit.");
+                    GameManagement.Instance.MissionFailed();
                 }
             }
         }
