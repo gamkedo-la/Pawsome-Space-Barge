1ce9ead82822158c1523de9cf201e9ac160a0838
Evan Sklarski <esklarski@gmail.com>
2022-04-23 03:45:21 -0700

    Clenup.

--- Assets/Scripts/UI/TextCrawler.cs
+++ Assets/Scripts/UI/TextCrawler.cs
@@ -1,19 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TextCrawler : MonoBehaviour
 {
     public float scrollSpeed = 20f;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.position;
