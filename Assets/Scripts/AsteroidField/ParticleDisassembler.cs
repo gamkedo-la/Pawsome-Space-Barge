600b71ec0112abadb1a2ab8a8fe0ea99795357db
Evan Sklarski <esklarski@gmail.com>
2022-04-27 12:48:11 -0700

    Initial testing of spawn/despawn zones on player ships. Tag barge collider to work with new layer.

--- Assets/Scripts/AsteroidField/ParticleDisassembler.cs
+++ Assets/Scripts/AsteroidField/ParticleDisassembler.cs
@@ -2,12 +2,23 @@
 
 public class ParticleDisassembler : MonoBehaviour
 {
+    [HideInInspector] private int layerMask;
+
+    private void Awake()
+    {
+        layerMask = LayerMask.GetMask("ActiveAsteroidZone");
+    }
+
     private void OnTriggerExit2D(Collider2D other)
     {
-        var pd = other.gameObject.GetComponent<ParticleDuality>();
-        if (pd != null)
+        if (!other.IsTouchingLayers(layerMask))
         {
-            pd.TurnIntoParticle();
+            ParticleDuality pd;
+
+            if (other.gameObject.TryGetComponent<ParticleDuality>(out pd))
+            {
+                pd.TurnIntoParticle();
+            }
         }
     }
 }
